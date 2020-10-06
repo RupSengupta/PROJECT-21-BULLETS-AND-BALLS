1eea3dd3185571d51292cb3dc2c34138fab2cdbe
RupSengupta <71700339+RupSengupta@users.noreply.github.com>
2020-10-06 10:32:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,70 +1,98 @@
-var wall, thickness;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var bullet, speed, weight;
-
-var deformation;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
 
 function setup() {
-  createCanvas(1600,800);
-  bullet =createSprite(50, 200, 50, 50);
+	createCanvas(800, 700);
+	rectMode(CENTER);
 
-  wall =createSprite(1200, 200, thickness, height/2);
+	packageSprite=createSprite(width/2, 200, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+	//packageSprite.visible=false;
 
-  speed=random(223,321)
-  weight=random(30,52)
-  thickness =random(22, 83);
+	
 
-  bullet.velocityX =speed;
+	
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.65, isStatic:false});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+	
+	Engine.run(engine);
+  
 }
+
 
 function draw() {
-  background("black");
+  rectMode(CENTER);
+  background(0);
+ 
+  
+ 
 
-
-
-if(hasCollided(bullet, wall))
-{
- bullet.velocityX=0;
- var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
-
-
- if(damage>10)
- {
-   wall.shapeColor="red";
- }
-
-
- if(damage<10)
- {
-   wall.shapeColor="green";
- }
-
-}
-
+  
+  
+  
+  keyPressed();
   drawSprites();
+ 
 }
 
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	// Look at the hints in the document and understand how to make the package body fall only on
+	//packageSprite.visible=true;
+	packageSprite.x= packageBody.position.x;
+	packageSprite.y= packageBody.position.y; 
 
-function hasCollided(bullet, wall) {
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
 
 
-  bulletRightEdge=bullet.x+bullet.width;
-  wallLeftEdge=wall.x;
-  if(bulletRightEdge>=wallLeftEdge)
-  {
+	
 
-    return true;
-  } else
-  return false;
+	
 
-
-
-
+	
+	
+	
+	
+  }
 }
-
-
-
-
 
 
 
