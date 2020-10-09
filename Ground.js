059422a432b4c80633879ec3f39c0c7f954ebcfb
RupSengupta <71700339+RupSengupta@users.noreply.github.com>
2020-10-09 22:02:40 +0530

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -1,30 +1,17 @@
-class Ground{
-
+class Ground {
     constructor(x,y,width,height) {
-    
-        var options={isStatic:true}
-    
-        this.body=Bodies.rectangle(x,y,width,height, options);
-
-        this.width=width;
-        this.height=height;
-
-        World.add(world,this.body);
-    
-    
-    
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
     }
-    
-    display() {
-    
-        var pos=this.body.position;
-        
-    
-        rectMode(CENTER);
-        fill("yellow");
-        rect(pos.x,pos.y,this.width,this.height);
-    
-    
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);
     }
-    
-    }
+  };
--- sketch.js
+++ sketch.js
@@ -1,73 +1,68 @@
-
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-var paper,ground;
-var side1, side2, side3;
 
+var engine, world;
+var box1, pig1;
+var backgroundImage;
+var dustbinImage;
+var side1;
+var dustbin1;
 
-function preload()
-{
-	
+function preload() {
+
+    
+    
+
 }
 
-function setup() {
-	createCanvas(800, 700);
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
 
-	
+    
+    ground = new Ground(600,height,1200,20)
 
+    dustbin1 =new Dustbin(800,370,200,200)
 
-	engine = Engine.create();
-	world = engine.world;
+    
 
-	//Create the Bodies Here.
- 
-
- side1=createSprite(width/1.8,height-28,200,20);
-	side1.shapeColor="white";
-
-	side2=createSprite(width/2.3,height-78,20,100);
-	side2.shapeColor="white";
-
-	side3=createSprite(width/1.48,height-78,20,100);
-	side3.shapeColor="white";
-
-	side1 =Bodies.rectangle(width/2,height-40,200,20,{isStatic:true});
-	 World.add(world,side1);
-
-	 side2 =Bodies.rectangle(width/2,height-15,20,100,{isStatic:true});
-	 World.add(world,side2);
-
-	 side3 =Bodies.rectangle(width/3,height-20,20,100,{isStatic:true});
-	 World.add(world,side3);
-
-	
-
-
-	paper=new Paper(110,660,40,40);
-	ground =new Ground(110,690,1400,20);
-
-
-
-	Engine.update(engine);
   
-}
 
-
-function draw() {
-  rectMode(CENTER);
-  background("black");
-  paper.display();
-  ground.display();
-
-  drawSprites();
  
+	
+
+	
+
+
+    
+   
+    
+    
+
+    
+
+    bird = new Bird(100,100);
+
 }
 
+function draw(){
+    background("grey");
 
+    Engine.update(engine);
+    bird.display();
+    ground.display();
+    dustbin1.display();
+    
+   
 
+    
+    
+   
 
+    
 
-
-
+    
+    drawSprites();
+}
